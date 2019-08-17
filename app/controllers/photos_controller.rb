25f8e28a4f536bfa0b5218feb3d5b404b40783c6
ttakuru88 <takuru@hai3.net>
2019-08-17 23:16:26 +0900

    アップロード画像を表示

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,16 @@ class PhotosController < ApplicationController
   def index
   end
 
+  @@detector = OpenCV::CvHaarClassifierCascade.load(Rails.root.join('lib', 'haarcascade_frontalface_alt.xml').to_s)
+
   def create
+    image = OpenCV::IplImage.load(params[:photo].path)
+
+    @@detector.detect_objects(image).each do |region|
+      # image.reset_roi
+    end
+
+    @image_path = "/images/#{SecureRandom.uuid}.png"
+    image.save_image(Rails.root.join("public/#{@image_path}").to_s)
   end
 end
