ea7a0514cc426f07860ea22b68978025ab2664ea
ttakuru88 <takuru@hai3.net>
2019-08-17 23:29:03 +0900

    顔認識して顔を貼り付ける

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,9 +6,15 @@ class PhotosController < ApplicationController
 
   def create
     image = OpenCV::IplImage.load(params[:photo].path)
+    face_image = OpenCV::IplImage.load(Rails.root.join('lib', 'icon_blue.png').to_s)
 
     @@detector.detect_objects(image).each do |region|
-      # image.reset_roi
+      resized_face = face_image.resize(region)
+      image.set_roi(region)
+      (resized_face.rows * resized_face.cols).times do |i|
+        image[i] = resized_face[i] if resized_face[i][0].to_i > 0 || resized_face[i][1].to_i > 0 || resized_face[i][2].to_i > 0
+      end
+      image.reset_roi
     end
 
     @image_path = "/images/#{SecureRandom.uuid}.png"
