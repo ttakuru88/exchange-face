eecd92421bc734b2c88b3d763dcf3f7e0b5443d1
ttakuru88 <takuru@hai3.net>
2019-08-18 13:59:48 +0900

    小さい顔は無視する

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,7 +8,7 @@ class PhotosController < ApplicationController
     image = OpenCV::IplImage.load(params[:photo].path)
     face_image = OpenCV::IplImage.load(Rails.root.join('lib', 'face.png').to_s)
 
-    @@detector.detect_objects(image).each do |region|
+    @@detector.detect_objects(image, min_size: OpenCV::CvSize.new(image.width * 0.05, image.height * 0.05)).each do |region|
       resized_face = face_image.resize(region)
       image.set_roi(region)
       (resized_face.rows * resized_face.cols).times do |i|
